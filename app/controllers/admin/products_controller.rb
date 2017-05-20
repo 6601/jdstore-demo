ae243325adfa4f7675f61fc9d88d2fd1c11087f7
jianbo <jjbob77@gmail.com>
2017-05-20 21:45:40 +0800

    implement backend CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,56 @@
+class Admin::ProductsController < ApplicationController
+	before_action :find_product_id, only:[:show, :edit, :update, :destroy]
+
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		
+	end
+
+	def show
+		
+	end
+
+
+	def update
+		if @product.update(product_params)
+			redirect_to admin_products_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@product.destroy
+		redirect_to admin_products_path
+	end
+
+
+
+	private
+
+	def find_product_id
+		@product = Product.find(params[:id])	
+	end
+
+	def product_params
+		params.require(:product).permit(:title, :description, :quantity, :price)
+		
+	end
+
+end
